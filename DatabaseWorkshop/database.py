3d493bd94b89bc19a1be5c460c741661e88d38e1
Nate Wren <nw1929@g.rit.edu>
2021-03-24 19:01:16 -0400

    Created function stubs

---
+++ DatabaseWorkshop/database.py
@@ -0,0 +1,27 @@
+#Database API
+#  Provides connection to the database on reddwarf to perform CRUD operations
+#
+#  Nate Wren
+
+host = "reddwarf.cs.rit.edu"
+user = "p320_01h"
+tables = ["person", "Request", "Tool"]
+
+def insert():
+    #TODO: implement
+
+def update():
+    #TODO: implement
+
+def delete():
+    #TODO: implement
+
+def connect():
+    #TODO: implement
+
+def main():
+    #TODO: implement
+    print("We're still working here... please bear with us...")
+
+if __name__ == "__main__":
+    main()
